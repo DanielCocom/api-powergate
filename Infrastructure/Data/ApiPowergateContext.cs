9f4f63c8ee3a2e337e4c0f35e5d44b3660c247aa
DanielCocom <danielalexdcd@gmail.com>
2025-07-24 22:03:41 -0600

    Se corrige conexion a db

--- Infrastructure/Data/ApiPowergateContext.cs
+++ Infrastructure/Data/ApiPowergateContext.cs
@@ -33,9 +33,7 @@ public partial class ApiPowergateContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=db_powergate;uid=root;pwd=dcd238e3", Microsoft.EntityFrameworkCore.ServerVersion.Parse("9.3.0-mysql"));
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Infrastructure/DependencyInjection/DependencyInjection.cs
+++ Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -3,6 +3,7 @@ using api_powergate.Domain.Interfaces;
 using api_powergate.Infrastructure.Data;
 using api_powergate.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
 
 namespace api_powergate.Infrastructure.DependencyInjection
@@ -11,15 +12,21 @@ namespace api_powergate.Infrastructure.DependencyInjection
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("ApiPowergateDbConnection");
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
             Console.WriteLine($"Cadena de conexión utilizada: {connectionString}");
 
             services.AddDbContext<ApiPowergateContext>(options =>
-                options.UseMySql(
-                    connectionString,
-                    new MySqlServerVersion(new Version(9, 3, 0))
-                )
-            );
+                options.UseMySql(connectionString,
+                    ServerVersion.AutoDetect(connectionString),
+                    mySqlOptions =>
+                    {
+                        mySqlOptions.EnableRetryOnFailure(
+                            maxRetryCount: 10,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null
+                        );
+                    }));
+
             services.AddCors(options =>
             {
                 options.AddPolicy("PermitirTodo", policy =>
@@ -30,17 +37,14 @@ namespace api_powergate.Infrastructure.DependencyInjection
                 });
             });
 
-
             // Repositorios
             services.AddScoped<ILecturaRepository, LecturaRepository>();
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IDispositivoRepository, DispositivoRepository>();
-
-
             services.AddScoped<ICanalDeCargaRepository, CanalDeCargaRepository>();
 
             // Servicios de aplicación
-            services.AddTransient<ILecturaService,LecturaService>();
+            services.AddTransient<ILecturaService, LecturaService>();
             services.AddTransient<IDispositivoService, DispositivoService>();
             services.AddTransient<AuthService>();
 
